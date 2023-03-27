b88625ca04078e8aece0e337e1b753f48d035641
AnnZ <nutazorro369@gmail.com>
2023-03-27 20:45:45 +0700

    Merge branch

--- H10/Урок 1. Практическое задание/task_1.py
+++ H10/Урок 1. Практическое задание/task_1.py
@@ -23,9 +23,9 @@ for line in words:
     print('содержание переменной - {}\n'.format(line))
     print('длинна строки: {}\n'.format(len(line)))
 print('-' * 50)
-points = [b'\xd1\x80\xd0\xb0\xd0\xb7\xd1\x80\xd0\xb0\xd0\xb1\xd0\xbe\xd1\x82\xd0\xba\xd0\xb0',
-          b'\xd1\x81\xd0\xbe\xd0\xba\xd0\xb5\xd1\x82',
-          b'\xd0\xb4\xd0\xb5\xd0\xba\xd0\xbe\xd1\x80\xd0\xb0\xd1\x82\xd0\xbe\xd1\x80']
+points = ['\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430',
+          '\u0441\u043e\u043a\u0435\u0442',
+          '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440']
 for line in points:
     print('тип переменной: {}\n'.format(type(line)))
     print('содержание переменной - {}\n'.format(line))
